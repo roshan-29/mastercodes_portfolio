2fad94be3d49711a121d285443480a665cb3c12a
roshan-29 <roshanvj29072002@gmail.com>
2023-06-10 23:05:53 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "MOVIEFLIX",
+    subtitle: "WEB DEVELOPEMENT PROJECT",
+    description:
+      "This is a web developement project using HTML CSS and JavaScript",
+    image: "./project-1.png",
+    link: "https://reactbootcamp.com",
+  },
+  
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Java",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  
+];
