49bed08beb90905a73daf6055d0fc90c0da5f734
roshan-29 <roshanvj29072002@gmail.com>
2023-06-12 23:37:08 +0530

    Inital commit

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="https://www.instagram.com/_rosha.nn/"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
               connect with me
             </a>
             <a
