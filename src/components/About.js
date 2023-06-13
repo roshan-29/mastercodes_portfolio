3edd93559f01383b3a1b35b77d6397d77e319dc3
roshan-29 <roshanvj29072002@gmail.com>
2023-06-13 19:07:48 +0530

    first-commit

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Roshan.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="hidden lg:inline-block" />I love to build amazing apps.
           </h1>
           <p className="mb-8 leading-relaxed">
            WELCOME to my PAGE!!
