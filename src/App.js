f743e2dd1636a6086b7a70d3249f912bc453c0e2
roshan-29 <roshanvj29072002@gmail.com>
2023-06-14 19:49:10 +0530

    image source chnaged

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
+
 
 export default function App() {
   return (
